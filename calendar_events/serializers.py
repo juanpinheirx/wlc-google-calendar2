2f14c1df3bcdad485e76543ae042c8f67bcf66d3
Juan Pinheiro <jroliveirapinheiro@gmail.com>
2024-09-13 22:43:49 -0300

    feat: configuradas as credenciais para API Google

---
+++ calendar_events/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class EventSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=200)
+    description = serializers.CharField()
+    date = serializers.DateField()
