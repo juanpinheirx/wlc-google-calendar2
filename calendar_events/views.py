392bcb1269ef0ed6aa9f5cd5e4d884dfbfd26eff
Juan Pinheiro <jroliveirapinheiro@gmail.com>
2024-09-13 23:44:29 -0300

    refactor: views.py ajustada com novas classes para listar e deletar.

--- calendar_events/views.py
+++ calendar_events/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, generics
 from .models import Task
 from .serializers import EventSerializer
 from .google_calendar import create_google_event, delete_google_event
@@ -6,17 +6,20 @@ from .google_calendar import create_google_event, delete_google_event
 # Create your views here.
 
 
-class TaskViewSet(viewsets.ModelViewSet):
+class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = EventSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ["title", "description", "date"]
 
     def perform_create(self, serializer):
         task = serializer.save()
         event_id = create_google_event(task)
         task.google_event_id = event_id
         task.save()
+
+
+class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = EventSerializer
 
     def perform_destroy(self, instance):
         delete_google_event(instance.google_event_id)
